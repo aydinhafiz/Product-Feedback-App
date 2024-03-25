361f7e270af8524f06cf4e252da0978e556371fe
Aydın Hafız <hafizaydin01@gmail.com>
2024-03-25 09:47:48 +0100

    working on useform controller

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -4,7 +4,7 @@ import { SLoginContainer } from "../components/login/login.styles";
 import axios from "axios";
 import { AuthContext } from "../contexts/auth-context";
 
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { useFormik } from "formik";
 
 function Login() {
@@ -20,9 +20,9 @@ function Login() {
   //   email: Yup.string().email("Invalid email").required("Required"),
   // });
 
-  const formik = useFormik({
-    initialValues: {
-      email: "aydin2@gmail.com",
+  const { handleSubmit, control } = useForm({
+    defaultValues: {
+      username: "aydin2@gmail.com",
       password: "123456",
     },
   });
@@ -46,49 +46,61 @@ function Login() {
       if (response.data.status === "success") {
         handleLoginState(response.data.token, response.data.user);
         setLoading(false);
+        console.log("HELLO");
       }
     } catch (error) {
       console.log(error);
       setLoading(false);
+      console.log("HELLO");
     }
   }
 
-  console.log(formik.errors);
-
-  // return (
-  //   <SLoginContainer onSubmit={handleSubmit(login)}>
-  //     <div className="login-page">
-  //       <h2 className="title-login">Login</h2>
-  //       <div className="login-info">
-  //         <input
-  //           className="login-email"
-  //           type="email"
-  //           name="email"
-  //           placeholder="Email address"
-  //           value={formik.values.email}
-  //           onChange={formik.handleChange}
-  //         />
-  //         <input
-  //           className="login-password"
-  //           name="password"
-  //           type="password"
-  //           placeholder="Password"
-  //           value={formik.values.password}
-  //           onChange={formik.handleChange}
-  //         />
-  //       </div>
-  //       <button disabled={loading} type="submit" className="login-your-account">
-  //         Login to your account
-  //       </button>
-  //       <h3 className="dont-have-account">
-  //         Don’t have an account?{" "}
-  //         <Link to={"/signup"}>
-  //           <span className="sign-up">Sign Up</span>
-  //         </Link>
-  //       </h3>
-  //     </div>
-  //   </SLoginContainer>
-  // );
+  return (
+    <SLoginContainer onSubmit={handleSubmit(login)}>
+      <Controller
+        control={control}
+        name="loginInput"
+        render={({ field: { onChange, onBlur, value, ref } }) => (
+          <div className="login-page">
+            <h2 className="title-login">Login</h2>
+            <div className="login-info">
+              <input
+                className="login-email"
+                type="email"
+                name="email"
+                placeholder="Email address"
+                onChange={onChange}
+                onBlur={onBlur}
+                value={value}
+              />
+              <input
+                className="login-password"
+                name="password"
+                type="password"
+                placeholder="Password"
+                onChange={onChange}
+                onBlur={onBlur}
+                value={value}
+              />
+            </div>
+            <button
+              disabled={loading}
+              type="submit"
+              className="login-your-account"
+            >
+              Login to your account
+            </button>
+            <h3 className="dont-have-account">
+              Don’t have an account?{" "}
+              <Link to={"/signup"}>
+                <span className="sign-up">Sign Up</span>
+              </Link>
+            </h3>
+          </div>
+        )}
+      />
+    </SLoginContainer>
+  );
 }
 
 export default Login;
